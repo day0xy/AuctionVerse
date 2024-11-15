626a0b140a4b99e3430bc99a5ca235ed981a1587
jason <jasonfan1337@gmail.com>
2024-11-15 09:59:21 +0800

    1

---
+++ src/Factory.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "./AuctionVerse.sol";
+
+contract AuctionVerse {
+    address[] public allAuctions;
+    mapping(address => address[]) public auctionsBySeller;
+
+    event AuctionCreated(address indexed seller, address auctionAddress);
+
+    function createAuction(
+        address fractionalizedRealEstateTokenAddress
+    ) external returns (address) {
+        AuctionVerse newAuction = new AuctionVerse(
+            fractionalizedRealEstateTokenAddress
+        );
+        allAuctions.push(address(newAuction));
+        auctionsBySeller[msg.sender].push(address(newAuction));
+
+        emit AuctionCreated(msg.sender, address(newAuction));
+        return address(newAuction);
+    }
+
+    function getAllAuctions() external view returns (address[] memory) {
+        return allAuctions;
+    }
+
+    function getAuctionsBySeller(
+        address seller
+    ) external view returns (address[] memory) {
+        return auctionsBySeller[seller];
+    }
+}
