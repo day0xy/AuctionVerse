6cd702f258caa51d27b2e3d5307a501fbe7370c6
jason <jasonfan1337@gmail.com>
2024-11-15 17:49:40 +0800

    1

--- src/AuctionVerse.sol
+++ src/AuctionVerse.sol
@@ -3,24 +3,32 @@ pragma solidity ^0.8.20;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import "./utils/Errors.sol";
+import {IERC1155Receiver, IERC165} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {Errors} from "./utils/Errors.sol";
 
-contract AuctionVerse is ReentrancyGuard {
+contract AuctionVerse is ReentrancyGuard, Errors {
     address internal immutable seller;
     address internal immutable atoken;
 
-    bool internal started;
-    uint256 internal startTimestamp; //起拍时间
-    uint48 internal endTimestamp; //结束时间
-    uint256 internal startedBid; //起拍价
-    uint256 internal bidDeposit; //起拍价押金
-    uint256 internal minimumIncrement; //最小加价幅度
-    uint256 internal incrementDuration; //加价幅度持续时间
-    uint256 internal duration; //持续时间，可以用block.timestamp-
-    address internal highestBidder; //最高出价者
-    uint256 internal highestBid; //最高出价
-    uint256 internal reservePrice; //保留价，如果最高价格没有达到保留价，卖家可以选择不卖
-    uint256 internal tokenIdOnAuction;
+    bool started;
+
+    uint256 startTimestamp; //起拍时间
+    uint48 endTimestamp; //结束时间
+    struct AuctionDetails {
+        uint256 startedBid; //起拍价
+        uint256 bidDeposit; //起拍价押金为起拍价的3%
+        uint256 Increment; //最小加价幅度
+        uint256 incrementDuration; //加价幅度持续时间
+        uint256 duration; //持续时间，可以用block.timestamp-
+        address highestBidder; //最高出价者
+        uint256 highestBid; //最高出价
+        uint256 reservePrice; //保留价，如果最高价格没有达到保留价，卖家可以选择不卖
+        uint256 tokenIdOnAuction;
+        uint256 fractionalizedAmountOnAuction;
+    }
+
+    AuctionDetails internal auctionDetails;
 
     mapping(address bidder => uint256 totalBiddedEth) internal bids;
 
@@ -40,7 +48,7 @@ contract AuctionVerse is ReentrancyGuard {
 
     constructor(address atokenAddress) {
         seller = msg.sender;
-        Atoken = atokenAddress;
+        atoken = atokenAddress;
     }
 
     function startAuction(
@@ -53,47 +61,46 @@ contract AuctionVerse is ReentrancyGuard {
         if (msg.sender != seller) revert AuctionVerse_OnlySellerCanCall();
 
         IERC1155(atoken).safeTransferFrom(
-            i_seller,
+            seller,
             address(this),
             tokenId,
             amount,
             data
         );
 
-        s_started = true;
-        s_endTimestamp = SafeCast.toUint48(block.timestamp + 7 days);
-        s_tokenIdOnAuction = tokenId;
-        s_fractionalizedAmountOnAuction = amount;
-        s_highestBidder = msg.sender;
-        s_highestBid = startingBid;
+        started = true;
+        endTimestamp = SafeCast.toUint48(block.timestamp + 7 days);
+        tokenIdOnAuction = tokenId;
+        fractionalizedAmountOnAuction = amount;
+        highestBidder = msg.sender;
+        highestBid = startingBid;
 
-        emit AuctionStarted(tokenId, amount, s_endTimestamp);
+        emit AuctionStarted(tokenId, amount, endTimestamp);
     }
 
     function getTokenIdOnAuction() external view returns (uint256) {
-        return s_tokenIdOnAuction;
+        return tokenIdOnAuction;
     }
 
     function bid() external payable nonReentrant {
-        if (!s_started) revert AuctionVerse_NoAuctionsInProgress();
-        if (block.timestamp >= s_endTimestamp)
-            revert AuctionVerse_AuctionEnded();
-        if (msg.value <= s_highestBid) revert AuctionVerse_BidNotHighEnough();
+        if (!started) revert AuctionVerse_NoAuctionsInProgress();
+        if (block.timestamp >= endTimestamp) revert AuctionVerse_AuctionEnded();
+        if (msg.value <= highestBid) revert AuctionVerse_BidNotHighEnough();
 
-        s_highestBidder = msg.sender;
-        s_highestBid = msg.value;
-        s_bids[msg.sender] += msg.value;
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        bids[msg.sender] += msg.value;
 
         emit Bid(msg.sender, msg.value);
     }
 
     function withdrawBid() external nonReentrant {
-        if (msg.sender == s_highestBidder)
+        if (msg.sender == highestBidder)
             revert AuctionVerse_CannotWithdrawHighestBid();
 
-        uint256 amount = s_bids[msg.sender];
+        uint256 amount = bids[msg.sender];
         if (amount == 0) revert NothingToWithdraw();
-        delete s_bids[msg.sender];
+        delete bids[msg.sender];
 
         (bool sent, ) = msg.sender.call{value: amount}("");
 
@@ -101,28 +108,27 @@ contract AuctionVerse is ReentrancyGuard {
     }
 
     function endAuction() external nonReentrant {
-        if (!s_started) revert AuctionVerse_NoAuctionsInProgress();
-        if (block.timestamp < s_endTimestamp)
-            revert AuctionVerse_TooEarlyToEnd();
+        if (!started) revert AuctionVerse_NoAuctionsInProgress();
+        if (block.timestamp < endTimestamp) revert AuctionVerse_TooEarlyToEnd();
 
-        s_started = false;
+        started = false;
 
-        IERC1155(i_fractionalizedRealEstateToken).safeTransferFrom(
+        IERC1155(atoken).safeTransferFrom(
             address(this),
-            s_highestBidder,
-            s_tokenIdOnAuction,
-            s_fractionalizedAmountOnAuction,
+            highestBidder,
+            tokenIdOnAuction,
+            fractionalizedAmountOnAuction,
             ""
         );
 
-        (bool sent, ) = i_seller.call{value: s_highestBid}("");
-        if (!sent) revert FailedToSendEth(i_seller, s_highestBid);
+        (bool sent, ) = seller.call{value: highestBid}("");
+        if (!sent) revert FailedToSendEth(seller, highestBid);
 
         emit AuctionEnded(
-            s_tokenIdOnAuction,
-            s_fractionalizedAmountOnAuction,
-            s_highestBidder,
-            s_highestBid
+            tokenIdOnAuction,
+            fractionalizedAmountOnAuction,
+            highestBidder,
+            highestBid
         );
     }
 
@@ -133,7 +139,7 @@ contract AuctionVerse is ReentrancyGuard {
         uint256 /*value*/,
         bytes calldata /*data*/
     ) external view returns (bytes4) {
-        if (msg.sender != address(i_fractionalizedRealEstateToken)) {
+        if (msg.sender != address(atoken)) {
             revert OnlyRealEstateTokenSupported();
         }
 
@@ -147,7 +153,7 @@ contract AuctionVerse is ReentrancyGuard {
         uint256[] calldata /*values*/,
         bytes calldata /*data*/
     ) external view returns (bytes4) {
-        if (msg.sender != address(i_fractionalizedRealEstateToken)) {
+        if (msg.sender != address(atoken)) {
             revert OnlyRealEstateTokenSupported();
         }
 
